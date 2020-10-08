5fa3d113b2a842e3fefe19f3bcdcfc7e70aebf51
Olia <oliakuku25@gmail.com>
2020-10-08 20:39:26 +0400

    added custumers list

---
+++ src/constants/index.js
@@ -0,0 +1,45 @@
+export const allClients = [
+    {
+        id: 1,
+        name: 'Sofya',
+        age: 22,
+        image:
+            'https://fanparty.ru/fanclubs/leverage/gallery/680723_leverage_pic.jpg',
+    },
+    {
+        id: 2,
+        name: 'Viktoria',
+        age: 18,
+        image: 'https://www.theplace.ru/archive/selena_gomez/img/latimes08.jpg',
+    },
+    {
+        id: 3,
+        name: 'Zhanna',
+        age: 29,
+        image:
+            'https://ru.hellomagazine.com/images/2015/june/nastya/ill02072015-4.jpg',
+    },
+    {
+        id: 4,
+        name: 'Nikolay',
+        age: 19,
+        image:
+            'https://images11.cosmopolitan.ru/upload/img_cache/e59/e599c41ef4c050b3a9ce228d9628b696_fitted_740x700.jpg',
+    },
+    {
+        id: 5,
+        name: 'Konstantin',
+        age: 35,
+        image:
+            'https://cs12.pikabu.ru/post_img/big/2019/09/03/10/1567531643294856999.jpg',
+    },
+    {
+        id: 6,
+        name: 'Aleksandr',
+        age: 27,
+        image: 'https://i.redd.it/9grbkarlhu821.jpg',
+    },
+]
+
+export const defaultImage =
+    'https://af20xx.bzexase.org/wp-content/uploads/2019/07/default-avatar-1276x1920.jpg'
